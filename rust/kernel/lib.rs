b7b67ddd205cce128ed950e25ad6d089688477ae
Miguel Ojeda <ojeda@kernel.org>
2023-03-01 00:48:52 +0100

    rust: kernel: add missing backticks in doc comment
    This is part of the effort to minimize the differences of the `rust`
    branch with respect to mainline in order to eventually drop it.
    
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -117,7 +117,7 @@ use core::marker::PhantomData;
 /// [`PAGE_SHIFT`]: ../../../include/asm-generic/page.h
 pub const PAGE_SIZE: usize = 1 << bindings::PAGE_SHIFT;
 
-/// Prefix to appear before log messages printed from within the kernel crate.
+/// Prefix to appear before log messages printed from within the `kernel` crate.
 const __LOG_PREFIX: &[u8] = b"rust_kernel\0";
 
 /// The top level entrypoint to implementing a kernel module.
