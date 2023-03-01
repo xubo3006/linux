7b82acd37895895a9314ebcc8620d4dfd1c89987
Miguel Ojeda <ojeda@kernel.org>
2023-03-01 03:33:42 +0100

    rust: concat_idents: use `format!` for concatenation
    This is part of the effort to minimize the differences of the `rust`
    branch with respect to mainline in order to eventually drop it.
    
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/macros/concat_idents.rs
+++ rust/macros/concat_idents.rs
@@ -18,6 +18,6 @@ pub(crate) fn concat_idents(ts: TokenStream) -> TokenStream {
     assert_eq!(expect_punct(&mut it), ',');
     let b = expect_ident(&mut it);
     assert!(it.next().is_none(), "only two idents can be concatenated");
-    let res = Ident::new(&(a.to_string() + &b.to_string()), b.span());
+    let res = Ident::new(&format!("{a}{b}"), b.span());
     TokenStream::from_iter([TokenTree::Ident(res)])
 }
